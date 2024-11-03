e8986393006b06447667edda0a4ae3db60b3e0fb
Aditya Patel <adpatel162@outlook.com>
2024-11-03 23:40:01 +0000

    false commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        background: "var(--fireground)",
         foreground: "var(--foreground)",
       },
     },
