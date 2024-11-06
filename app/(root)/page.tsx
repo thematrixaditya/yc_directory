243b11f4a1b536194db462b01cd08fc96a608036
Aditya Patel <adpatel162@outlook.com>
2024-11-06 02:30:07 +0000

    started designing the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <>Home</>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entreprenuers</h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote On Pitches , And Get Noticed By Competitions.
+        </p>
+      </section>
+    </>
   );
 }
