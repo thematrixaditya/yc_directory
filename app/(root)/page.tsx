72a497b4ff21c2871ef32d4dd45d571a1da9c823
Aditya Patel <adpatel162@outlook.com>
2024-11-06 16:34:26 +0000

    implemented the ui and basic functionality for search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
+  const query = (await searchParams).query;
+
   return (
     <>
       <section className="pink_container">
@@ -7,6 +11,8 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote On Pitches , And Get Noticed By Competitions.
         </p>
+
+        <SearchForm query={query} />
       </section>
     </>
   );
